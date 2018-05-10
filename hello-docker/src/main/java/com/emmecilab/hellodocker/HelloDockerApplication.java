f7130a2830c2ae6c086c45e6060047a580e980af
Mauro Cicolella <mcicolella@libero.it>
2018-05-10 16:30:42 +0200

    Add hello-docker example

---
+++ hello-docker/src/main/java/com/emmecilab/hellodocker/HelloDockerApplication.java
@@ -0,0 +1,12 @@
+package com.emmecilab.hellodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDockerApplication.class, args);
+	}
+}
---
+++ hello-docker/src/test/java/com/emmecilab/hellodocker/HelloDockerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.emmecilab.hellodocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloDockerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
