70077852f44fe6d0d8db3c1cb37acdb2f3b0922d
Mauro Cicolella <mcicolella@libero.it>
2020-07-11 16:43:29 +0200

    Create install-debian10.sh

---
+++ installation/debian/install-debian10.sh
@@ -0,0 +1,23 @@
+# Update system and install base packages
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+# Add the Docker repository to Debian 10
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+# Install Docker & Docker Compose on Debian 10
+sudo apt update
+sudo apt -y install docker-ce docker-ce-cli containerd.io
+
+# Add your user to run docker commands as non-privileged user
+sudo usermod -aG docker $USER
+newgrp docker
+
+# Check docker versione
+docker version
