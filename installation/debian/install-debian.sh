99bb6ecb5784295cddf0fc876d0ee5f101209b68
Mauro Cicolella <mcicolella@libero.it>
2020-07-13 21:28:00 +0200

    Add docker-compose installation

--- installation/debian/install-debian.sh
+++ installation/debian/install-debian.sh
@@ -18,3 +18,7 @@ newgrp docker
 # Check docker version
 docker version
 
+#Download docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
