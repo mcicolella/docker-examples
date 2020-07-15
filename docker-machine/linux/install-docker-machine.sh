ea12889b3d2a852eaecb31d5a23d83c106433ead
Mauro Cicolella <mcicolella@libero.it>
2020-07-15 17:32:52 +0200

    Update install-docker-machine.sh

--- docker-machine/linux/install-docker-machine.sh
+++ docker-machine/linux/install-docker-machine.sh
@@ -3,5 +3,5 @@ apt-get install unzip curl docker.io
 service docker stop
 echo manual | sudo tee /etc/init/docker.override
 curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
-chmod +x /tmp/docker-machine &&
+chmod +x /tmp/docker-machine
 sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
