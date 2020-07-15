cd83421a24a6d17228602d8ac30c84117b5bc571
Mauro Cicolella <mcicolella@libero.it>
2020-07-15 17:31:11 +0200

    Delete install-docker-machine.sh

--- docker-machine/linux/install-docker-machine.sh
+++
@@ -1,7 +0,0 @@
-apt-get update
-apt-get install unzip curl docker.io
-service docker stop
-echo manual | sudo tee /etc/init/docker.override
-curl -L https://github.com/docker/machine/releases/download/v0.16.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine &&
-chmod +x /tmp/docker-machine &&
-sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
