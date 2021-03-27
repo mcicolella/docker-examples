cfc16ddc888ae08f5f41f1f824c8a9170e9996f0
Mauro Cicolella <mcicolella@libero.it>
2021-03-27 15:19:51 +0100

    Add ctop installation script

---
+++ tools/ctop/install_ctop.sh
@@ -0,0 +1,7 @@
+# ctop
+# https://github.com/bcicen/ctop
+
+echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
+wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
+sudo apt update
+sudo apt install docker-ctop
