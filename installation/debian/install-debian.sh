45c9ec48f546d1be5f8788872dcfe109c8dabb1a
Mauro Cicolella <mcicolella@libero.it>
2021-04-24 15:17:53 +0200

    Update install-debian.sh

--- installation/debian/install-debian.sh
+++ installation/debian/install-debian.sh
@@ -26,4 +26,5 @@ docker -v
 # Download docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
