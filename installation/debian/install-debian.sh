51b648746352c0ae8553316ce3e3124e2f5ed870
Mauro Cicolella <mcicolella@libero.it>
2021-04-24 15:02:13 +0200

    Update install-debian.sh

--- installation/debian/install-debian.sh
+++ installation/debian/install-debian.sh
@@ -7,13 +7,12 @@ sudo apt -y install apt-transport-https ca-certificates curl gnupg lsb-release
 # Add Docker’s official GPG key
  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# Add the Docker repository to Debian 10
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+# Add the Docker repository 
+ echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Install Docker & Docker Compose on Debian 10
+# Install Docker & Docker Compose 
 sudo apt update
 sudo apt -y install docker-ce docker-ce-cli containerd.io
 
