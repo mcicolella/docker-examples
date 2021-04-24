bf0de2e5ac0b62e58ddf27288b00a75a82c27b7c
Mauro Cicolella <mcicolella@libero.it>
2021-04-24 14:21:20 +0200

    Update install-debian.sh

--- installation/debian/install-debian.sh
+++ installation/debian/install-debian.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
+
 # Update system and install base packages
 sudo apt update
 sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
 # Add Docker’s official GPG key
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
 # Add the Docker repository to Debian 10
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
+
 # Install Docker & Docker Compose on Debian 10
 sudo apt update
 sudo apt -y install docker-ce docker-ce-cli containerd.io
+
 # Add your user to run docker commands as non-privileged user
 sudo usermod -aG docker $USER
 newgrp docker
+
 # Check docker version
 docker version
 
-#Download docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Download docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
