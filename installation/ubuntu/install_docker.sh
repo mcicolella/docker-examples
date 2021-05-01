8d84ff151dbf2ec78b44062c6d1d9231c08e0255
Mauro Cicolella <mcicolella@libero.it>
2021-05-01 11:12:45 +0200

    Create install_docker.sh

---
+++ installation/ubuntu/install_docker.sh
@@ -0,0 +1,26 @@
+# Update system
+sudo apt update
+
+# Install a few prerequisite packages which let apt use packages over HTTPS
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+# A the GPG key for the official Docker repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+ 
+# Add the Docker repository to APT sources
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+ 
+# Update the package database with the Docker packages from the newly added repository
+sudo apt update
+ 
+# Make sure you are about to install from the Docker repository instead of the default Ubuntu repository
+apt-cache policy docker-ce
+
+# Install Docker Community Edition
+sudo apt install docker-ce
+ 
+# Check if the Docker daemon is running
+sudo systemctl status docker
+
+# Check Docker version
+sudo docker -v 
